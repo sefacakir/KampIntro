cc3f6fa1cc852c3fe5bbbddef9fabf105f275582
Sefa <cakirsefa72@gmail.com>
2021-09-17 15:10:34 +0300

    16 Ocak ders sonu.

---
+++ Methods/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyatı = 15;
+            string aciklama = "Amasya Elması";
+
+            string[] meyveler = new string[] { };
+
+            Product urun1 = new Product();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Product urun2 = new Product();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Product[] urunler = new Product[] { urun1, urun2};
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine("----------------");
+            }
+
+            Console.WriteLine("-------------Metotlar-----------");
+
+            //encapsulation, parametrede değerler yerine komple ürünün kendisini göndermesi
+            //parça parça yazmak yerine bir kapsülleme yapıp, kapsülü gönderiyoruz.
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil armut", 12, 10);
+            sepetManager.Ekle2("Elma", "Yeşil Elma", 12, 9);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır karpuzu", 12,8);
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Methods/SepetManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    class SepetManager
+    {
+        public void Ekle(Product urun)
+        {
+            Console.WriteLine("Sepete eklendi : "+urun.Adi);
+             
+        }
+        public void Ekle2(string urunAd, string aciklama, double fiyat, int stockAdedi)
+        {
+            Console.WriteLine("Tebrikler! Ürün eklendi: "+urunAd);
+        }
+    }
+}
