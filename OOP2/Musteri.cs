87cceccd6cc36c830bae658f8277e59daa96ce37
Sefa <cakirsefa72@gmail.com>
2021-09-17 23:52:48 +0300

    5. Gün ilk mola.

---
+++ OOP2/Musteri.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class Musteri
+    {
+        //Müşteri classından miras alan tüzelmüşteri ve 
+        //gerçekmüşteri, müşteri sınıfının içerisindeki
+        //özellikleri de miras alır.
+        public int Id { get; set; }
+        public string MusteriNo { get; set; }
+
+
+
+
+
+    }
+}
